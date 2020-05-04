4b37ab838815b950a9b790e2560e8c4ce3b98384
Emiliano Alvarez <MetalEmi7@gmail.com>
2020-05-03 21:24:06 -0300

    Rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { MenuComponent } from './pages/menu/menu.component';
+import { pathToFileURL } from 'url';
+import { ListadoComponent } from './pages/listado/listado.component';
 
 const routes: Routes = [
   //Rutas
-  { path: "", component: HomeComponent },
+  {
+    path: "", component: MenuComponent,
+    children: [
+      { path: "Listado", component: ListadoComponent },
+      
+    ]
+  },
   { path: "", component: HomeComponent },
   { path: "", component: HomeComponent },
   { path: "", component: HomeComponent },
